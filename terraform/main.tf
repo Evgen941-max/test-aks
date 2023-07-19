5b5e3a48a2ff99bde06bd4a13d01544065a80d4b
Evgen941-max <Kolomijchyk@hotmail.com>
2023-07-19 17:52:21 +0300

    11

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-        source          = "hashicorp/azurerm"
-        version         = "2.48.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name          = "leark8sResourceGroup"
   location      = "northeurope"
